9de39740d3be5650153402bd056d7ce452891345
Dhruti Avadhani <dhruti.avadhani2004@gmail.com>
2026-05-12 07:26:15 +0000

    Initial FireOps SOC structure

---
+++ setup-scripts/install_wazuh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+LOGFILE=~/fireops-soc/docs/wazuh-install.log
+
+echo "[+] Starting FireOps SOC setup..." | tee -a $LOGFILE
+
+echo "[+] Installing dependencies..." | tee -a $LOGFILE
+sudo apt install curl unzip git nano apt-transport-https lsb-release -y 2>&1 | tee -a $LOGFILE
+
+echo "[+] Downloading Wazuh installer..." | tee -a $LOGFILE
+curl -sO https://packages.wazuh.com/4.7/wazuh-install.sh 2>&1 | tee -a $LOGFILE
+
+echo "[+] Installing Wazuh..." | tee -a $LOGFILE
+sudo bash ./wazuh-install.sh -a 2>&1 | tee -a $LOGFILE
+
+echo "[+] Installation completed. Refer logs for degubbing ><" | tee -a $LOGFILE
